6b48ed30435765ddfdcb1edfdcd22241df39092c
oliverswitzer <oliverswitzer@gmail.com>
2014-07-21 20:48:04 -0400

    set serve_static_assets to true in prod for js pages file

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
   describe "new_color" do
+    it "fetches a new color from the ColorQueue class" do
 
+    end
   end
 end
